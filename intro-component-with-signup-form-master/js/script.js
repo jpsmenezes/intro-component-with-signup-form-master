2c0035bddc88f8af0bdd51106f062acd9281c09d
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-01-31 13:13:51 -0300

    Update script.js

--- intro-component-with-signup-form-master/js/script.js
+++ intro-component-with-signup-form-master/js/script.js
@@ -1,7 +1,18 @@
 let validator = {
        handleSubmit: (event) =>{
               event.preventDefault(); // não enviar formulário
-       
+
+              let send = true;
+              const input = form.querySelector('input');
+
+              let check = validator.checkInput(input);
+              if (check !== true) {
+                  send = false;
+                  validator.showError(input, check);
+              }
+              if (send) {            
+                  form.submit();
+              }
        }
 }
 
