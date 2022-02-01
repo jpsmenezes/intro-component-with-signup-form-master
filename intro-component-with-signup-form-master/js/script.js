c70dc4ec2afdbdeee553225d7e9fbc7a9a5d67c2
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-02-01 11:59:06 -0300

    html and js

--- intro-component-with-signup-form-master/js/script.js
+++ intro-component-with-signup-form-master/js/script.js
@@ -13,14 +13,39 @@ let validator = {
             if (check !== true) {
                 send = false;
                 validator.showError(input, check);
-            }            
+            }
         }
         if (send) {
             form.submit();
         }
     },
     checkInput: (input) => {
-
+        let rules = input.getAttribute('data-rules');
+        rules = rules.split('|');
+        for (let k in rules) {
+            let rDetails = rules[k].split('=');
+            switch (rDetails[0]) {
+                case 'required':
+                    if (input.value == "") {
+                        return 'Campo Obrigatório'
+                    }
+                    break;
+                case 'min':
+                    if (input.value.length < rDetails[1]) {
+                        return 'Obrigatório ter ao menos ' + rDetails[1] + ' Caracteres';
+                    }
+                    break;
+                case 'email':
+                    if (input.value != '') {
+                        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                        if (!regex.test(input.value.toLowerCase())) {
+                            return 'E-mail digitado não é válido!';
+                        }
+                    }
+                    break;
+            }
+        }
+        return true;
     },
     showError: (input, erro) => {
 
